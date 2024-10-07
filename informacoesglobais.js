88d0cbee34adc4f1bd3048fe146f8503ecb2e3df
Lucashenrique0000 <144020843+Lucashenrique0000@users.noreply.github.com>
2024-10-07 11:05:50 -0300

    Create informacoesglobais.js

---
+++ informacoesglobais.js
@@ -0,0 +1,18 @@
+async function quantidadeUsuarios () {
+    const url:'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios'
+    json'
+    const res = await fecth (url) 
+    const dados = await res.json ()
+   const nomeDasRedes = Object.keys(dados)
+   const quantidadeUsuarios = Object.values(dados)
+   
+   const data
+    {
+       x: nomeDasRedes,
+       y:quantidadeUsuarios,
+       type:' bar'
+   
+    }
+   }
+   
+    quantidadeUsuarios()
